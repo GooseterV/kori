c27f1427fd0ffd12670abbe1e53c267359f2bb05
Gus V <dangoenjoyer@gmail.com>
2022-12-16 14:35:12 -0500

    test

--- helpers/ai.js
+++ helpers/ai.js
@@ -1,12 +1,18 @@
+/* eslint-disable no-undef */
 const loadEnv = require("dotenv").config;
 loadEnv();
 
 async function makeQuery(q) {
+	
 	const ChatGPTAPI = await (await import("chatgpt")).ChatGPTAPI;
-	const api = new ChatGPTAPI({
-		// eslint-disable-next-line no-undef
-		sessionToken: process.env.SESSION_TOKEN
+	const getOpenAIAuth = await (await import("chatgpt")).getOpenAIAuth;
+
+	const auth = await getOpenAIAuth({
+		email: process.env.OPENAI_EMAIL,
+		password: process.env.OPENAI_PASSWORD
 	});
+
+	const api = new ChatGPTAPI(...auth);
 	await api.ensureAuth();
 	const response = await api.sendMessage(
 		q
