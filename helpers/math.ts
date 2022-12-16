e73d10a40044350eb95b81fd8adabd5f53ece378
Gus V <dangoenjoyer@gmail.com>
2022-12-15 21:36:13 -0500

    updates

--- helpers/math.ts
+++ helpers/math.ts
@@ -1,11 +1,11 @@
-function factorial(n:number) {
+function factorial(n:number):number{
 	if (n === 0) {
 		return 1;
 	}
 	return n * factorial(n - 1);
 }
 
-function log(n:number, base:number) {
+function log(n:number, base:number){
 	return Math.log(n) / Math.log(base);
 }
 
